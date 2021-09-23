965fb582af0c186fc9341f915041cb7c573c0aeb
cynthia <glean@cynthiateeters.com>
2021-09-23 13:51:13 -0400

    Fix spelling errors

--- src/resistor.js
+++ src/resistor.js
@@ -1,9 +1,9 @@
 /* resistor.js */
 
 /*
-  elctronic resistors have colored bands where each color represents a numerical number
+  electronic resistors have colored bands where each color represents a numerical number
   Electrical engineers can read the colors and
-  determine the resitance value in Ohms
+  determine the resistance value in Ohms
 
   see this resistor calculator for help
    http://bit.ly/2NjS274
